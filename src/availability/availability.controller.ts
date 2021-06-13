a3c51139ab19a71fbe4c1fa7ee4d1bc3a55aa236
Vivek Joy <joyvivek93@gmail.com>
2021-06-13 20:46:43 +0530

    api service and controller for getting centers by latitude and longitude

--- src/availability/availability.controller.ts
+++ src/availability/availability.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AvailabilityService } from './availability.service';
 
 @Controller('availability')
 export class AvailabilityController {
   constructor(private readonly availabilityService: AvailabilityService) {}
 
-  @Get()
-  findAll() {
-    return this.availabilityService.findAll();
+  @Get('centers/latLong')
+  findCentersByLocation(
+    @Query('lat') lat: string,
+    @Query('long') long: string,
+  ) {
+    const latitude = Number(lat).toFixed(2);
+    const longitude = Number(long).toFixed(2);
+
+    return this.availabilityService.findCentersByLocation(latitude, longitude);
   }
 }
--- src/availability/availability.service.ts
+++ src/availability/availability.service.ts
@@ -1,8 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService, Injectable } from '@nestjs/common';
+import { map } from 'rxjs/operators';
+import { CacheService } from 'src/cache.service';
+import { CACHE_KEYS } from 'src/utils/constants';
 
 @Injectable()
 export class AvailabilityService {
+  constructor(
+    private httpService: HttpService,
+    private cacheService: CacheService,
+  ) {}
+
   findAll() {
     return `This action returns all availability`;
+  }
+
+  async findCentersByLocation(lat: string, long: string) {
+    const CACHE_STORE_KEY = CACHE_KEYS.CENTERS_BY_LATLONG.replace(
+      '{lat}-{long}',
+      `${lat}-${long}`,
+    );
+
+    try {
+      const cacheResponse = await this.cacheService.getFromCache(
+        CACHE_STORE_KEY,
+      );
+      if (cacheResponse) {
+        console.log('FROM CACHE', CACHE_STORE_KEY);
+        return cacheResponse;
+      }
+      return this.httpService
+        .get(
+          `/appointment/centers/public/findByLatLong?lat=${lat}&long=${long}`,
+        )
+        .pipe(
+          map((response) => {
+            this.cacheService.setResponseToCache(
+              CACHE_STORE_KEY,
+              response.data,
+            );
+            return response.data;
+          }),
+        );
+    } catch (err) {
+      throw Error(err);
+    }
   }
 }
